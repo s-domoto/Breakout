2104fa6c99f769e12bcb51c5c5bbbd591a140dd3
domoto <koremori0315@yahoo.co.jp>
2018-11-13 21:19:51 +0900

    Breakout 新規コミット

---
+++ Breakout/Breakout/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Breakout
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.score = new System.Windows.Forms.Label();
+            this.textBoxScore = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButtonEasy = new System.Windows.Forms.RadioButton();
+            this.radioButtonNormal = new System.Windows.Forms.RadioButton();
+            this.radioButtonHard = new System.Windows.Forms.RadioButton();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Location = new System.Drawing.Point(372, 13);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(38, 12);
+            this.score.TabIndex = 0;
+            this.score.Text = "とくてん";
+            // 
+            // textBoxScore
+            // 
+            this.textBoxScore.Location = new System.Drawing.Point(412, 10);
+            this.textBoxScore.Name = "textBoxScore";
+            this.textBoxScore.ReadOnly = true;
+            this.textBoxScore.Size = new System.Drawing.Size(100, 19);
+            this.textBoxScore.TabIndex = 1;
+            this.textBoxScore.TabStop = false;
+            this.textBoxScore.Text = "0";
+            this.textBoxScore.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonStart);
+            this.panel1.Controls.Add(this.radioButtonHard);
+            this.panel1.Controls.Add(this.radioButtonNormal);
+            this.panel1.Controls.Add(this.radioButtonEasy);
+            this.panel1.Location = new System.Drawing.Point(1, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(523, 50);
+            this.panel1.TabIndex = 2;
+            // 
+            // radioButtonEasy
+            // 
+            this.radioButtonEasy.AutoSize = true;
+            this.radioButtonEasy.Location = new System.Drawing.Point(11, 11);
+            this.radioButtonEasy.Name = "radioButtonEasy";
+            this.radioButtonEasy.Size = new System.Drawing.Size(62, 16);
+            this.radioButtonEasy.TabIndex = 3;
+            this.radioButtonEasy.Text = "かんたん";
+            this.radioButtonEasy.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonNormal
+            // 
+            this.radioButtonNormal.AutoSize = true;
+            this.radioButtonNormal.Location = new System.Drawing.Point(71, 11);
+            this.radioButtonNormal.Name = "radioButtonNormal";
+            this.radioButtonNormal.Size = new System.Drawing.Size(49, 16);
+            this.radioButtonNormal.TabIndex = 4;
+            this.radioButtonNormal.Text = "ふつう";
+            this.radioButtonNormal.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonHard
+            // 
+            this.radioButtonHard.AutoSize = true;
+            this.radioButtonHard.Location = new System.Drawing.Point(119, 11);
+            this.radioButtonHard.Name = "radioButtonHard";
+            this.radioButtonHard.Size = new System.Drawing.Size(72, 16);
+            this.radioButtonHard.TabIndex = 5;
+            this.radioButtonHard.Text = "むずかしい";
+            this.radioButtonHard.UseVisualStyleBackColor = true;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(195, 8);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 6;
+            this.buttonStart.TabStop = false;
+            this.buttonStart.Text = "すたーと！";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 441);
+            this.Controls.Add(this.textBoxScore);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "ブロック崩しゲーム";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Draw);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PaddleMove);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.TextBox textBoxScore;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButtonHard;
+        private System.Windows.Forms.RadioButton radioButtonNormal;
+        private System.Windows.Forms.RadioButton radioButtonEasy;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ Breakout/Breakout/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace Breakout
+{
+    public partial class Form1 : Form
+    {
+        Vector ballPos;
+        Vector ballSpeed;
+        int ballRadius;
+        Rectangle paddlePos;
+        List<Rectangle> blockPos;
+        Timer timer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // パドルの初期位置とサイズ
+            this.paddlePos = new Rectangle(100, this.Height - 50, 100, 5);
+            this.blockPos = new List<Rectangle>();
+            // ブロックをリストに格納
+            for(int x = 0; x <= this.Height; x += 100)
+            {
+                for(int y = 50; y <= 200; y += 40)
+                {
+                    this.blockPos.Add(new Rectangle(25 + x, y, 80, 25));
+                }
+            }
+        }
+
+        double DotProduct(Vector a, Vector b)
+        {
+            // 内積計算
+            return a.X * b.X + a.Y * b.Y;
+        }
+
+        bool PaddleHitCircle(Vector p1, Vector p2, Vector center, float radius)
+        {
+            // パドルの方向ベクトル
+            Vector paddleDir = (p2 - p1);
+            // パドルの法線
+            Vector n = new Vector(paddleDir.Y, -paddleDir.X);
+            n.Normalize();
+
+            Vector dir1 = center - p1;
+            Vector dir2 = center - p2;
+
+            double dist = Math.Abs(DotProduct(dir1, n));
+            double a1 = DotProduct(dir1, paddleDir);
+            double a2 = DotProduct(dir2, paddleDir);
+
+            return (a1 * a2 < 0 && dist < radius) ? true : false;
+        }
+
+        int BlockHitCircle(Rectangle block, Vector ball)
+        {
+            if (PaddleHitCircle(new Vector(block.Left, block.Top),
+                new Vector(block.Right, block.Top), ball, ballRadius))
+            {
+                return 1;
+            }
+            else if (PaddleHitCircle(new Vector(block.Left, block.Bottom),
+               new Vector(block.Right, block.Bottom), ball, ballRadius))
+            {
+                return 2;
+            }
+            else if (PaddleHitCircle(new Vector(block.Right, block.Top),
+               new Vector(block.Right, block.Bottom), ball, ballRadius))
+            {
+                return 3;
+            }
+            else if (PaddleHitCircle(new Vector(block.Left, block.Top),
+                new Vector(block.Left, block.Bottom), ball, ballRadius))
+            {
+                return 4;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+            // ボールの移動
+            ballPos += ballSpeed;
+
+            // 左右の壁でのバウンド
+            if (ballPos.X + ballRadius > this.Bounds.Width - ballRadius || ballPos.X - ballRadius < 0)
+            {
+                ballSpeed.X *= -1;
+            }
+
+            // 上の壁でバウンド
+            if (ballPos.Y - ballRadius < 50)
+            {
+                ballSpeed.Y *= -1;
+            }
+
+            // ボールが下に落ちたとき
+            if (ballPos.Y == this.Height)
+            {
+                MessageBox.Show("げーむおーばー" + "\r\n" + "あなたのとくてんは：" + textBoxScore.Text);
+                this.Dispose();
+                Application.Restart();
+            }
+
+            // ブロックが無くなったとき
+            if(blockPos.Count == 0)
+            {
+                timer.Stop();
+                MessageBox.Show("げーむくりあ！" + "\r\n" + textBoxScore.Text);
+                this.Dispose();
+                Application.Restart();
+            }
+
+            // パドルのあたり判定
+            if (PaddleHitCircle(new Vector(this.paddlePos.Left, this.paddlePos.Top),
+                             new Vector(this.paddlePos.Right, this.paddlePos.Top),
+                             ballPos, ballRadius))
+            {
+                ballSpeed.Y *= -1;
+            }
+
+            // ブロックとのあたり判定
+            for(int i = 0; i < this.blockPos.Count; i++)
+            {
+                int collision = BlockHitCircle(blockPos[i], ballPos);
+                if (collision == 1 || collision == 2)
+                {
+                    ballSpeed.Y *= -1;
+                    this.blockPos.Remove(blockPos[i]);
+                    textBoxScore.Text = (int.Parse(textBoxScore.Text) + 10).ToString() ;
+                }
+                else if (collision == 3 || collision == 4)
+                {
+                    ballSpeed.X *= -1;
+                    this.blockPos.Remove(blockPos[i]);
+                    textBoxScore.Text = (int.Parse(textBoxScore.Text) + 10).ToString();
+                }
+
+            }
+
+            // 再描画
+            Invalidate();
+        }
+
+        private void Draw(object sender, PaintEventArgs e)
+        {
+            SolidBrush pinkBrush = new SolidBrush(Color.HotPink);
+            SolidBrush grayBrush = new SolidBrush(Color.DimGray);
+            SolidBrush blueBrush = new SolidBrush(Color.LightBlue);
+
+
+            float px = (float)this.ballPos.X - ballRadius;
+            float py = (float)this.ballPos.Y - ballRadius;
+
+            e.Graphics.FillEllipse(pinkBrush, px, py, this.ballRadius * 2, this.ballRadius * 2);
+            e.Graphics.FillRectangle(grayBrush, paddlePos);
+
+            for(int i = 0; i < blockPos.Count; i++)
+            {
+                e.Graphics.FillRectangle(blueBrush, blockPos[i]);
+            }
+        }
+
+        private void PaddleMove(object sender, KeyPressEventArgs e)
+        {
+            // Aキーが押された時
+            if (e.KeyChar == 'a')
+            {
+                this.paddlePos.X -= 20;
+            }
+            // Sキーが押された時
+            if (e.KeyChar == 's')
+            {
+                this.paddlePos.X += 20;
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // ボールの初期位置を設定
+            this.ballPos = new Vector(200, 240);
+            // ボールの半径を設定
+            this.ballRadius = 10;
+
+            // かんたんモード
+            if (radioButtonEasy.Checked == true)
+            {
+                this.ballSpeed = new Vector(-2, -4);
+            }
+            // ふつうモード
+            else if (radioButtonNormal.Checked == true)
+            {
+                this.ballSpeed = new Vector(-4, -6);
+            }
+            // むずかしいモード
+            else if (radioButtonHard.Checked == true)
+            {
+                this.ballSpeed = new Vector(-6, -10);
+            }
+            // 30ミリ秒ごとに画面を再描画
+            timer.Interval = 30;
+            timer.Tick += new EventHandler(Update);
+            timer.Start();
+        }
+    }
+}
